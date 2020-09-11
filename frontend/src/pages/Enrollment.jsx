d81a567a7d6b810f1f50177eb1d34bf0211b9e9b
ramyabyyu <bintantramy@gmail.com>
2020-09-11 14:58:11 +0700

    Creating Header for Enrollment Page

--- frontend/src/pages/Enrollment.jsx
+++ frontend/src/pages/Enrollment.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Header from "../components/Header";
+import { Button } from "react-bootstrap";
 
 function Enrollment() {
-  return <div>Pendaftaran</div>;
+  return (
+    <div>
+      <Header
+        title="Pendaftaran Academy"
+        subtitle="Jadwal belajar Academy dimulai tanggal 21 Februari 2021"
+      >
+        <p>Anda perlu registrasi Akun untuk mendaftar</p>
+        <Button href="/auth/register" size="lg" variant="outline-secondary">
+          Daftar Sekarang
+        </Button>
+      </Header>
+    </div>
+  );
 }
 
 export default Enrollment;
