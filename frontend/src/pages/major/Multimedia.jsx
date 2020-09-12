6c2bba415a89684cf126fef044700528b73dee7b
ramyabyyu <bintantramy@gmail.com>
2020-09-12 10:16:39 +0700

    <FrontEnd> Finishing Multimedia Page and Fixing css template

--- frontend/src/pages/major/Multimedia.jsx
+++ frontend/src/pages/major/Multimedia.jsx
@@ -1,7 +1,231 @@
 import React from "react";
+import Header from "../../components/Header";
+import Title from "../../components/Title";
 
 function Multimedia() {
-  return <div>Multimedia</div>;
+  return (
+    <div>
+      <Header
+        title="Multimedia"
+        subtitle="Ilmu Design Grafis, Editing, Animasi, dan lain-lain"
+      />
+
+      <section className="article">
+        <div className="section-1">
+          <Title title="Apa Itu Multimedia?" />
+          <p className="lead">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
+            totam delectus, voluptate nihil autem eos, dignissimos a explicabo
+            laboriosam expedita similique in et id eligendi aliquid beatae
+            necessitatibus provident saepe, voluptatum nemo modi! Voluptate
+            harum modi, laboriosam voluptas animi voluptatem rem, dolorum,
+            asperiores velit sapiente optio at suscipit provident quidem eos!
+            Eius quos ut nesciunt tenetur? Odio libero impedit, maxime nam
+            maiores aspernatur nisi veritatis expedita temporibus. Et sit
+            accusamus minus at reiciendis maiores facilis magni ratione natus,
+            cupiditate placeat dignissimos possimus nisi architecto soluta ipsam
+            quisquam! Sapiente esse quibusdam minima ipsam nemo error ea a
+            impedit cum porro quae tempore odit ipsum, id laboriosam eos laborum
+            perspiciatis, expedita mollitia numquam officiis eligendi dicta
+            doloremque? Suscipit eum provident hic dolores optio. Lorem ipsum
+            dolor sit, amet consectetur adipisicing elit. Nihil error non
+            possimus quisquam obcaecati, culpa mollitia doloribus maiores eius
+            quam eos natus dolorum similique pariatur tempora! Quo molestias
+            odio possimus!
+          </p>
+        </div>
+
+        <div className="section-2">
+          <Title title="Apa Yang dipelajari di Jurusan Ini?" />
+          <h6>Design Grafis dan Percetakan</h6>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+            placeat, aliquid est dolor, delectus nam cum esse veritatis commodi,
+            quos nulla modi deleniti. Asperiores placeat minus sit dignissimos
+            modi unde architecto nam molestias illo libero, tempore,
+            consequuntur amet? Esse quasi ex odit, fugiat minima vel soluta
+            optio dolores illum, ratione perferendis quod. Iure itaque culpa nam
+            alias beatae at suscipit. Ipsum neque ducimus itaque. Est omnis
+            culpa, quae ratione illo obcaecati dolorum cumque assumenda
+            reiciendis nisi consectetur rem optio minima temporibus error quis
+            voluptatibus placeat quaerat porro, consequuntur aut nobis.
+          </p>
+          <h6>Photography</h6>
+          <p className="lead">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae
+            molestias mollitia aliquam! Alias sit maxime quae numquam, facere
+            corrupti eveniet! Placeat nisi voluptate est aliquid, recusandae
+            consectetur. Possimus molestias eligendi doloremque! Veniam, rerum
+            ratione vitae iste totam, necessitatibus officiis quidem sequi iusto
+            architecto repellendus aspernatur ut assumenda modi praesentium
+            veritatis numquam quae excepturi voluptatem fugiat debitis earum at.
+            Laborum blanditiis velit dicta, quibusdam aliquid sit totam hic cum
+            ratione quo. Optio ad quas est, deserunt culpa reprehenderit
+            assumenda, aliquid beatae quisquam at fugit sapiente suscipit eius
+            perspiciatis pariatur ipsam odio quaerat vero, repellat eum
+            consequuntur vel nostrum? Quo saepe nisi illum quaerat pariatur sed
+            animi.
+          </p>
+
+          <h6>Web Design</h6>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A vel aut,
+            hic repudiandae iure dolore, odio voluptatem rem illum
+            necessitatibus facere eligendi totam temporibus nobis modi iste.
+            Sequi voluptas nisi explicabo deleniti expedita? Molestiae quibusdam
+            totam sunt recusandae quod eum doloribus cupiditate exercitationem
+            earum dolorem corporis ipsam ex inventore ipsa est, sit blanditiis
+            odit, veniam consequuntur iste sequi voluptate fugit distinctio.
+            Pariatur eveniet quidem sunt eos consectetur! Veniam qui repellendus
+            consequatur illo voluptatibus mollitia accusantium reprehenderit
+            excepturi ipsa architecto? Animi natus, nam expedita, excepturi
+            magni nobis ea, voluptate velit facere eos quibusdam? Dignissimos at
+            mollitia cumque minima nulla et, quos perspiciatis molestiae
+            quisquam tempore ipsa atque ducimus itaque vero inventore fugit
+            voluptas omnis, illum ea.
+          </p>
+
+          <h6>Animasi</h6>
+          <p className="lead">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum nisi
+            ea sapiente nihil adipisci maxime voluptas accusantium optio
+            aspernatur molestias aliquam aliquid dicta repudiandae officia iure,
+            magnam reprehenderit voluptates cumque ipsam dolorum veritatis animi
+            ullam facere dolore. Neque qui, cupiditate libero assumenda
+            voluptatibus quisquam excepturi ducimus quasi provident fuga ipsum
+            sed consectetur nulla facere natus molestias voluptate? Delectus
+            praesentium tempora voluptates voluptatum. In beatae placeat maxime
+            sequi asperiores error animi vero labore. Facere sapiente id eius
+            qui assumenda dolorum magni reiciendis odit fuga maxime, architecto
+            iure expedita reprehenderit sequi adipisci nihil nobis? Veniam atque
+            voluptate nam sunt.
+          </p>
+
+          <h6>Pengolahan Video, Audio, dan Gambar</h6>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi vel,
+            fuga harum eaque ex consequuntur doloribus? Fuga dolor autem,
+            delectus architecto similique obcaecati itaque accusamus! Molestiae
+            voluptas at est, nobis laborum cupiditate aliquam harum sint eum
+            voluptatibus debitis! A distinctio ea exercitationem ut doloremque
+            autem impedit voluptatibus magni minima. Totam perspiciatis
+            cupiditate, numquam nulla veniam minima ullam vel vitae, ipsa
+            temporibus quaerat eaque perferendis libero quam! Velit totam
+            possimus nemo cumque voluptatum rerum similique repellat at fuga
+            quaerat, eveniet cum iste consequuntur facilis labore nihil animi
+            voluptates ipsam perspiciatis asperiores ut? Ab suscipit vitae
+            accusantium iure, assumenda voluptatibus repellendus quisquam
+            necessitatibus expedita nulla sunt a sint sit iste? Asperiores
+            provident exercitationem sunt voluptatem amet iusto aliquid nostrum
+            nulla consectetur inventore ea, quam veritatis autem accusamus
+            quaerat tempora! Consequuntur iure impedit veritatis?
+          </p>
+        </div>
+
+        <div className="section-3">
+          <Title title="Prospek Kerja Jurusan Multimedia?" />
+
+          <h6>Designer</h6>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure iusto
+            vero porro. Et quisquam at eum ullam iusto inventore reprehenderit
+            non necessitatibus in natus, eaque deleniti tempore ab repudiandae
+            esse facere odit similique assumenda debitis molestias, fuga
+            asperiores voluptas. Laborum vitae dolorum ea et quam voluptas quod
+            similique alias itaque, accusantium ipsum dignissimos voluptatibus,
+            iste voluptatum eum facilis deserunt a incidunt nulla cum veritatis
+            aspernatur in obcaecati? Laudantium perferendis unde tenetur
+            provident autem cupiditate quidem quam ipsum atque quas libero sunt
+            laboriosam dolorem, quos non minima culpa sint dicta.
+          </p>
+
+          <h6>Video Editor</h6>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas at
+            fugit ratione, dolore ipsam quisquam vitae cupiditate nisi
+            recusandae iste rerum, vero consectetur reprehenderit mollitia.
+            Porro autem earum mollitia corporis odit, corrupti illo quo,
+            architecto necessitatibus, ducimus similique quis. Voluptas quos
+            cupiditate explicabo alias ullam nesciunt deleniti porro eius
+            corporis unde pariatur quaerat dolore, exercitationem neque sunt
+            similique. Repellat deleniti sapiente molestias autem esse
+            praesentium fugit atque cumque natus, inventore quidem corporis
+            maiores totam culpa libero fuga voluptas tenetur eius dignissimos
+            soluta? Sapiente eaque exercitationem recusandae vitae nesciunt
+            beatae id.
+          </p>
+
+          <h6>Animator</h6>
+          <p className="lead">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis
+            voluptas voluptatum quos voluptates! Eius consequuntur quo ipsam
+            facilis accusantium eos laborum eveniet est doloremque. Distinctio
+            minus aut culpa explicabo doloremque! Sequi saepe error debitis
+            tenetur dolor! Rem voluptates architecto molestiae. Ipsam nihil
+            consequatur delectus magni quos perferendis libero voluptatum
+            cumque, omnis corrupti vel. Distinctio similique voluptatibus illum
+            aspernatur quod! Dolores molestias cum libero impedit blanditiis
+            perferendis sit error distinctio nam ipsum sunt fugiat illo, nihil,
+            dolore vitae suscipit neque id necessitatibus expedita quis. Iusto
+            nisi fugiat reprehenderit rem voluptas itaque totam perspiciatis
+            aspernatur, deleniti alias cupiditate accusantium officia
+            perferendis fugit.
+          </p>
+
+          <h6>Music Composer</h6>
+          <p className="lead">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
+            optio corrupti molestiae hic, ratione quasi labore deserunt minus
+            consectetur accusantium culpa et itaque eveniet voluptatibus a, quas
+            assumenda omnis nobis deleniti suscipit numquam quam repellat. In
+            nihil recusandae repellendus dignissimos dolorum? Totam accusantium
+            voluptate sunt culpa cumque ipsam? Corrupti maiores id consectetur
+            earum iure quibusdam quia veritatis molestiae, dolorem, cum
+            architecto. Iusto necessitatibus, qui expedita numquam veritatis
+            voluptatem eos explicabo ea id fugit. Mollitia corporis fuga debitis
+            non ducimus consectetur, amet qui. Voluptas nulla inventore non
+            quibusdam sit praesentium dignissimos error aliquam vel provident?
+            Aut nobis est optio voluptatem illum perferendis reiciendis
+            doloribus molestiae quas?
+          </p>
+
+          <h6>UI/UX Designer</h6>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+            maxime quisquam. Officiis quas, id quis iure ut recusandae expedita
+            eveniet rem libero quasi iusto assumenda cumque rerum reiciendis
+            vero nisi distinctio eius perspiciatis quaerat ducimus. Sint
+            cupiditate nam tempora incidunt sequi magni omnis neque laudantium.
+            Magni illum fugiat corporis officia, suscipit in, placeat libero
+            inventore quam optio ipsa illo! Doloremque minus nostrum at magnam
+            iure facilis deserunt architecto delectus earum quas neque atque, ut
+            quasi, et sed! A rem alias consequuntur est voluptatem eligendi.
+            Debitis quod quis tenetur vero ut doloremque esse dolore quam hic
+            voluptates maiores exercitationem unde natus, in, velit, aspernatur
+            adipisci fuga molestias aperiam quisquam! Accusamus quod incidunt,
+            temporibus adipisci modi sunt magnam. Esse molestias adipisci est?
+          </p>
+
+          <h6>Photographer</h6>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            aliquam recusandae, velit assumenda, minima repellat laboriosam est,
+            odit nihil ipsa sed ullam. Aliquid fugiat aspernatur blanditiis
+            ratione laudantium officiis dolores maxime libero, ad nemo ipsum
+            facere nam illo minima? Iure repellendus expedita voluptatibus,
+            facilis, magni quisquam nobis consectetur culpa sapiente quae ullam
+            impedit reprehenderit officia aliquam minima tempora incidunt
+            accusamus minus molestiae nesciunt doloremque doloribus earum! Porro
+            quos eum necessitatibus, totam magnam atque alias esse neque culpa
+            quis natus tempore! Quod consectetur quisquam necessitatibus nihil
+            magnam cupiditate culpa deserunt dolorem asperiores ea, dignissimos
+            ex vitae animi possimus consequuntur, nulla facilis quaerat dolore
+            tempora doloremque quas laboriosam eligendi. Iure, ratione tempora!
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Multimedia;
