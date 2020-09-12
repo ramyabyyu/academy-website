02e54c354cc5ecea62de98ef00634bb039b0ee3d
ramyabyyu <bintantramy@gmail.com>
2020-09-12 08:07:29 +0700

    <FrontEnd> Finishing VisiMisi Page

--- frontend/src/pages/profile/Visi.jsx
+++ frontend/src/pages/profile/Visi.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import Header from "../../components/Header";
+import Title from "../../components/Title";
 
 function Visi() {
-  return <div>Visii</div>;
+  return (
+    <div>
+      <Header
+        title="Visi Misi"
+        subtitle="Apa Sajakah Visi dan Misi Academy Web?"
+      />
+
+      <section className="visi-misi">
+        <Title title="Visi Misi" />
+
+        <h3>Visi</h3>
+        <p className="lead">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint suscipit
+          pariatur aut necessitatibus, sed esse et tenetur earum repellendus
+          explicabo est iusto id ab, quo adipisci libero sit a quisquam animi
+          assumenda expedita vitae dolorum ea cumque. Ea dignissimos unde
+          dolorem, dolorum inventore delectus iste optio illum impedit fugiat
+          dolore magni officia repellendus iure nesciunt quasi libero
+          reprehenderit atque, corrupti aut. Aliquam possimus beatae tempora
+          enim corporis, est totam deserunt quae vel, nam commodi illum ipsa
+          modi, minus sit? Nam.
+        </p>
+
+        <h3>Misi</h3>
+        <p className="lead">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa libero
+          suscipit eos aperiam rerum. Ipsa, itaque incidunt ducimus ullam magni
+          tenetur veniam consectetur, voluptatem temporibus eveniet ex deserunt
+          nihil! Ad, sint ratione odit facere, quas fugit modi qui eos at esse
+          harum animi reprehenderit recusandae mollitia cupiditate nulla neque,
+          provident quo? Earum qui eligendi quae ea delectus, at et officiis
+          nostrum, ullam, molestiae ut expedita numquam debitis neque eveniet
+          dolorum quidem similique! Accusantium, alias optio. Officia natus
+          cumque ducimus nulla nobis porro deleniti, dignissimos dicta
+          distinctio molestias facilis nam architecto consequuntur quasi minus
+          culpa impedit.
+        </p>
+      </section>
+    </div>
+  );
 }
 
 export default Visi;
